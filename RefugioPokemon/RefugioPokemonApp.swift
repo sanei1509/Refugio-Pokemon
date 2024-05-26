af8d01f4856b6d57335bc5a42246b9a4b3f2c49a
santiago <santiagoneira52@gmail.com>
2024-05-26 14:07:00 -0300

    Initial Commit

---
+++ RefugioPokemon/RefugioPokemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  RefugioPokemonApp.swift
+//  RefugioPokemon
+//
+//  Created by Santiago Neira on 5/26/24.
+//
+
+import SwiftUI
+
+@main
+struct RefugioPokemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
